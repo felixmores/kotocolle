64e9dda7cb848766896b91b306e3ffa4fa1c3933
kakedashi <livingforce4250@yahoo.co.jp>
2018-10-22 10:15:01 +0000

    Set send mail config

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.mail.yahoo.co.jp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'galuda05ex@yahoo.co.jp'),
+        'name' => env('MAIL_FROM_NAME', 'kotocolle'),
     ],
 
     /*
