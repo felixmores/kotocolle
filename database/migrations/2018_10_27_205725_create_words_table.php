7062f5945ec993cf685126def83f8af79f854de5
kakedashi <livingforce4250@yahoo.co.jp>
2018-10-27 13:32:19 +0000

    Create words and comments table

---
+++ database/migrations/2018_10_27_205725_create_words_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('word');
+            $table->index('word');
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->smallInteger('lank')->nullable();
+            $table->index('lank');
+            $table->text('memo')->nullable();
+            $table->string('word_image')->nullable();
+            $table->smallInteger('share_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
